f337d8c0aeaf722b7f6b083aabc988c10d333730
NiNja <ninja@ninja.com>
2019-11-01 22:32:48 +0200

    Add tslint.

--- game-box-mobile-ui/src/app/app-routing.module.ts
+++ game-box-mobile-ui/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { Routes, PreloadAllModules } from '@angular/router';
 
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+
 ];
 
 @NgModule({
-    imports: [NativeScriptRouterModule.forRoot(routes)],
-    exports: [NativeScriptRouterModule]
+  imports: [
+    NativeScriptRouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })
+  ],
+  exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule { }
