b0b8677a4cd6dac7378200370bc71a07a6663705
NiNja <ninja@ninja.com>
2019-11-02 13:23:07 +0200

    Refactored jwt interceptor

--- game-box-web-ui/src/app/interceptors/jwt.interceptor.ts
+++ game-box-web-ui/src/app/interceptors/jwt.interceptor.ts
@@ -10,20 +10,20 @@ import {
 
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-import { ToastrService } from 'ngx-toastr';
 
+import { ToastrService } from 'ngx-toastr';
 import { AuthHelperService } from '../modules/core/services/auth-helper.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   constructor(
     private router: Router,
-    private toastr: ToastrService,
+    private toastrService: ToastrService,
     private authService: AuthHelperService
   ) { }
 
   public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const currentUser = this.authService.user;
 
     if (currentUser && currentUser.token) {
       request = request.clone({
@@ -38,36 +38,20 @@ export class JwtInterceptor implements HttpInterceptor {
       .handle(request)
       .pipe(
         tap((res: any) => {
-          if (res instanceof HttpResponse
-            && res.body
-            && res.body.token
-            && (res.url.endsWith('login')
-              || res.url.endsWith('register'))) {
-            this.saveToken(res.body);
-            this.toastr.success(res.body.message, 'Success!');
-            this.router.navigate(['/']);
-          }
-
-          if (res instanceof HttpResponse
-            && res.body
-            && res.body.message
-            && !res.url.endsWith('login')
-            && !res.url.endsWith('register')) {
-            this.toastr.success(res.body.message, 'Success!');
-          }
-
-          if (res instanceof HttpResponse
-            && res.body
-            && !res.url.endsWith('login')
-            && !res.url.endsWith('register')
-            && typeof res.body === 'string') {
-            this.toastr.success(res.body, 'Success!');
+          if (res instanceof HttpResponse && res.body) {
+            if (res.body.token && (res.url.endsWith('login') || res.url.endsWith('register'))) {
+              this.authService.user = res.body;
+              this.toastrService.success(res.body.message, 'Success!');
+              this.router.navigate(['/']);
+            } else if (!res.url.endsWith('login') && !res.url.endsWith('register')) {
+              if (typeof res.body === 'string') {
+                this.toastrService.success(res.body, 'Success!');
+              } else if (res.body.message) {
+                this.toastrService.success(res.body.message, 'Success!');
+              }
+            }
           }
         })
       );
-  }
-
-  private saveToken(data: any): void {
-    this.authService.user = data;
   }
 }
