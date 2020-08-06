6aae0765fbc709205d4f1622d4d8efd52ba1716a
TodorStamenov <toshostamenov@gmail.com>
2020-08-06 16:02:41 +0300

    Use strict templates for web UI project

--- Client/game-box-web-ui/src/app/modules/core/pipes/sliceString.pipe.ts
+++ Client/game-box-web-ui/src/app/modules/core/pipes/sliceString.pipe.ts
@@ -1,18 +1,12 @@
-import { PipeTransform, Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'sliceString'
 })
 export class SliceStringPipe implements PipeTransform {
-  private defaultLength = 200;
-
-  transform(value: string, length: number) {
-    if (length && length < value.length) {
+  public transform(value: string, length: number = 200): string {
+    if (0 < length && length < value.length) {
       return value.substring(0, length) + '...';
-    }
-
-    if (!length && this.defaultLength < value.length) {
-      return value.substring(0, this.defaultLength) + '...';
     }
 
     return value;
