737d96dcb6b647362f2966373f498458c6149bb8
TodorStamenov <toshostamenov@gmail.com>
2022-02-17 22:49:41 +0200

    Use Flutter for mobile UI

--- Client/game_box_admin_ui/android/app/src/main/kotlin/org/ninja/game_box_admin_ui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.ninja.game_box_admin_ui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ Client/game_box_mobile_ui/android/app/src/main/kotlin/com/example/game_box_mobile_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.game_box_mobile_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
