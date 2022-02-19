48521cbd906c6b22375baffcead297075997af4d
TodorStamenov <toshostamenov@gmail.com>
2022-02-19 17:50:45 +0200

    Added stateful widget

--- Client/game_box_mobile_ui/lib/main.dart
+++ Client/game_box_mobile_ui/lib/main.dart
@@ -6,36 +6,65 @@ void main() => runApp(
       ),
     );
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Color(0xff343A40),
         title: Text(
-          'Title Text',
+          'Title',
           style: TextStyle(
             fontSize: 18,
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
         ),
-        centerTitle: true,
-        backgroundColor: Color(0xff343a40),
       ),
-      body: Center(
-        child: Text(
-          'Hello world!',
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 50),
+            child: Text(
+              'Counter Value: ${this.counter}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('Click'),
-        onPressed: () => print('clicked!'),
-        backgroundColor: Color(0xff343a40),
+          Expanded(
+            child: Center(
+              child: MaterialButton(
+                color: Color(0xff343A40),
+                padding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                child: Text(
+                  'Click me!',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: () => setState(() => this.counter++),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
